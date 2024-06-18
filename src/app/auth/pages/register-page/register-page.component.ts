b38d60bc43d2d16b4de13e2260215de64be12de2
Julian Montufar <julamontufar@misena.edu.co>
2024-06-18 14:59:54 -0500

    Se realiza validaciones en el register

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['',[Validators.required]],
+    email: ['',[Validators.required]],
+    username: ['',[Validators.required, cantBeStrider]],
+    password: ['',[Validators.required, Validators.minLength(6)]],
+    password2: ['',[Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor( private fb: FormBuilder) {}
+
+  isValidField(){
+    //TODO obtener validacion desde un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
