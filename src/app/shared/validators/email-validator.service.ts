c28d4137ab4b98ada2179d650ff4cfc78d4c5f3b
Julian Montufar <julamontufar@misena.edu.co>
2024-06-18 19:48:40 -0500

    se agregan validadores asincronos en el emails

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -2,20 +2,56 @@ import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
 import { Observable, delay, of } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class EmailValidator implements AsyncValidator {
-    
+
     validate(control: AbstractControl): Observable<ValidationErrors | null> {
-        
+
         const email = control.value;
 
-        console.log(email)
 
-        return of({
-            emailTaken: true
+
+        const httpCallObservable = new Observable<ValidationErrors | null>((susbscriber) => {
+            console.log({ email });
+
+            if (email === 'fernando@google.com') {
+                susbscriber.next({ emailTaken: true });
+                susbscriber.complete();
+                //return;
+            }
+
+            susbscriber.next(null);
+            susbscriber.complete();
+
         }).pipe(
-            delay(2000)
-        )
+            delay(3000)
+        );
+
+        return httpCallObservable;
     }
+    // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
+    //     const email = control.value;
+
+    //     console.log(email)
+
+    //     return of({
+    //         emailTaken: true
+    //     }).pipe(
+    //         delay(2000)
+    //     )
+    // }
+
+
 
 }
+
+// return this.http.get<any[]>(`http://localhost:3000/users?q=${ email }`)
+// .pipe(
+//   // delay(3000),
+//   map( resp => {
+//     return ( resp.length === 0 )
+//         ? null
+//         : { emailTaken: true }
+//   })
+// );
